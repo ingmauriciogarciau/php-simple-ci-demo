746d5e20be14385889788048eaf8e5beaba3570c
ingmauriciogarcia <mauriciogarcia@sensunapan.com>
2025-10-24 18:46:34 -0600

    4

--- .test/BasicTest.php
+++ .test/BasicTest.php
@@ -8,7 +8,7 @@ class BasicTest extends TestCase
     {
         // 5 + 3 debe ser igual a 8
         //prueba mala
-        $resultado = sumar(10, 10);
+        $resultado = sumar(1, 10);
         $this->assertEquals(20, $resultado);
     }
 }
