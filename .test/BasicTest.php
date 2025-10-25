42dc87fe5af002acf71cc5360f063039674c4791
ingmauriciogarcia <mauriciogarcia@sensunapan.com>
2025-10-24 18:26:03 -0600

    trece

---
+++ .test/BasicTest.php
@@ -0,0 +1,14 @@
+<?php
+use PHPUnit\Framework\TestCase;
+require_once __DIR__ . '/../index.php';
+
+class BasicTest extends TestCase
+{
+    public function testLaFuncionSumaCorrectamente()
+    {
+        // 5 + 3 debe ser igual a 8
+        //prueba mala
+        $resultado = sumar(4, 4);
+        $this->assertEquals(8, $resultado);
+    }
+}
